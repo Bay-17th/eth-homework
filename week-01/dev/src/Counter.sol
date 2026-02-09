3fa3b219828d518da87c1cb0d8274cff100cacee
백우헌 <m1k-2y@users.noreply.github.com>
2026-02-09 18:28:37 +0900

    feat(week-01): complete counter assignment

--- week-01/dev/src/Counter.sol
+++ week-01/dev/src/Counter.sol
@@ -30,6 +30,7 @@ contract Counter {
     /// @notice 카운트를 1 증가시킵니다
     /// @dev count 값을 1만큼 증가시키는 로직을 구현하세요
     function increment() public {
+	count += 1;
         // TODO: count를 1 증가시키세요
         // 힌트: count += 1; 또는 count = count + 1; 또는 count++;
     }
@@ -37,6 +38,8 @@ contract Counter {
     /// @notice 카운트를 1 감소시킵니다
     /// @dev count가 0일 때 감소시키면 언더플로우가 발생합니다
     function decrement() public {
+	require(count > 0, "Count cannot go below zero");
+	count -= 1; 
         // TODO: count를 1 감소시키세요. 단, count가 0이면 revert해야 합니다.
         // 힌트: require(조건, "에러 메시지"); 를 사용하세요
         // 힌트: require(count > 0, "Count cannot go below zero");
@@ -45,6 +48,7 @@ contract Counter {
     /// @notice 카운트를 0으로 초기화합니다
     /// @dev count 값을 0으로 설정하는 로직을 구현하세요
     function reset() public {
+	count = 0;
         // TODO: count를 0으로 초기화하세요
         // 힌트: count = 0;
     }
