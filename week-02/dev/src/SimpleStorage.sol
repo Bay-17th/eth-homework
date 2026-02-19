ff58c8197dd58b2c9a013cd6f9d0b3a52c3cd58f
kignoh <kenya1218@naver.com>
2026-02-19 16:52:53 +0900

    feat(week-02): complete SimpleStorage.sol, quiz-02

--- week-02/dev/src/SimpleStorage.sol
+++ week-02/dev/src/SimpleStorage.sol
@@ -52,6 +52,8 @@ contract SimpleStorage {
         // 힌트:
         // balances[msg.sender] += msg.value;
         // emit Deposited(msg.sender, msg.value);
+        balances[msg.sender] += msg.value;
+        emit Deposited(msg.sender, msg.value);
     }
 
     /// @notice ETH를 출금합니다
@@ -69,5 +71,9 @@ contract SimpleStorage {
         // balances[msg.sender] -= amount;
         // payable(msg.sender).transfer(amount);
         // emit Withdrawn(msg.sender, amount);
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+        emit Withdrawn(msg.sender, amount);
     }
 }
